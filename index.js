b52856e02d2b7caec66ba4c59d06bef1f6f0a46d
Dr Paul Brewer <drpaulbrewer@eaftc.com>
2021-04-28 19:38:10 -0400

    return Number.NaN for NaN cases

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ module.exports = function(data){
 	}
 	f = function(x){
         if (typeof(x)!=='number')  throw new TypeError('cdf function input must be a number, got:'+typeof(x));
-		if (Number.isNaN(x)) return x;
+		if (Number.isNaN(x)) return Number.NaN;
 	    var left=0, right=xs.length-1, mid, midval;
 	    if (x<xs[0]) return 0;
 	    if (x>=xs[xs.length-1]) return 1;
